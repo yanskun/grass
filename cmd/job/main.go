36166ecec7156db65406ca245929d7cdb89dc197
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-03-23 22:55:14 +0900

    feat: main.go

--- cmd/job/main.go
+++ cmd/job/main.go
@@ -1,22 +1,64 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/google/go-github/v43/github"
 	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
+	"github.com/yasudanaoya/grass/internal/application"
+	"golang.org/x/oauth2"
 )
+
+type SlackMessage struct {
+	Text string `json:"text"`
+}
 
 func main() {
 	app := &cli.App{
 		Action: func(c *cli.Context) error {
-			println("Hello World!")
+			ctx := context.Background()
 			err := godotenv.Load()
+
 			if err != nil {
 				log.Fatal("Error loading .env file")
 			}
-			_ = os.Getenv("GITHUB_TOKEN")
+			token := os.Getenv("GITHUB_TOKEN")
+			url := os.Getenv("WEBHOOK_URL")
+
+			ts := oauth2.StaticTokenSource(
+				&oauth2.Token{AccessToken: token},
+			)
+			tc := oauth2.NewClient(ctx, ts)
+			client := github.NewClient(tc)
+
+			srv := application.NewContributeAppSrv(*client)
+			cmd := application.ContributeAppSrvCmd{}
+
+			result, err := srv.Get(ctx, cmd)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var message string
+
+			if result {
+				message = "今日は commit しました。"
+			} else {
+				message = "今日まだ commit していません。"
+			}
+			byte, err := json.Marshal(SlackMessage{Text: message})
+
+			http.Post(
+				url,
+				"aplication/json",
+				bytes.NewBufferString(string(byte)),
+			)
 
 			return nil
 		},
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-github/v41 v41.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
@@ -25,8 +26,11 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
